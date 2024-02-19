0cea1fa6aeb7c1ecc4ddd8dcbd92424a0daedcda
Cristina Correa <cristina.correa.segade@gmail.com>
2024-02-19 13:31:57 +0100

    Renderizado de listas map

--- Apuntes/midudev/Ejercicio 01/src/App.jsx
+++ Apuntes/midudev/Ejercicio 01/src/App.jsx
@@ -4,6 +4,7 @@ import TwitterCardInicialMejorada from "./components/TwitterCardInicialMejorada"
 import TwitterCardInicialChildren from "./components/TwitterCardInicialChildren";
 import TwitterFollowCard from "./components/TwitterFollowCard";
 import { useState } from "react";
+import TwitterFollowCardAdvanced from "./components/TwitterFollowCardAdvanced";
 
 function App() {
   const addAt = (userName) => `@${userName}`;
@@ -14,6 +15,50 @@ function App() {
 
   // Estado de prueba
   const [name, setName] = useState("midudev");
+
+  // Renderizado de listas
+  const users = [
+    {
+      userName: "midudev",
+      name: "Miguel Ángel Durán",
+      isFollowing: true
+    },
+    {
+      userName: "TraversyMedia",
+      name: "Traversy Media",
+    isFollowing: false
+    },
+    {
+      userName: "carlosAzaustre",
+      name: "Carlos Azaustre",
+      isFollowing: true
+    },
+    {
+      "userName": "Codecourse",
+      "name": "Codecourse",
+      "isFollowing": false
+    },
+  {
+    "userName": "Academind",
+    "name": "Academind",
+    "isFollowing": false
+  },
+  {
+    "userName": "freeCodeCamp",
+    "name": "freeCodeCamp",
+    "isFollowing": true
+  },
+  {
+    "userName": "CodeBullet",
+    "name": "Code Bullet",
+    "isFollowing": false
+  },
+  {
+    "userName": "edureka",
+    "name": "Edureka",
+    "isFollowing": false
+  }
+  ]
 
   return (
     <section className="App">
@@ -45,6 +90,19 @@ function App() {
       <button onClick={() => setName("CrisCorreaS")}>
         Cambio de nombre
       </button>
+      <hr />
+      {/* Renderizado de listas */}
+      {
+        users.map(({ userName, name, isFollowing }) => ( // Cuidado que esto es un paréntesis y no abrimos llaves, si no, no se interpreta como un return
+          <TwitterFollowCardAdvanced
+            key={userName} // La key es el identificador único del componente y se utiliza para diferenciar los componentes mapeados 
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCardAdvanced>
+        ))
+      }
     </section>
   );
 }
